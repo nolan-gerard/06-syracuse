0cb8554d9361edd43ed094961dbd4fd5abbc2588
nolan-gerard <nolan.gerard@edu.esiee.fr>
2024-11-28 09:05:39 +0000

    Voilà mon rendu. Nolan GERARD

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+"""Bonjour, on verra ici un nouvel exercice de Syracuse.
+"""
 #### Fonctions secondaires
 
 
 # imports
 from plotly.graph_objects import Scatter, Figure
-
-### NE PAS MODIFIER ###
 def syr_plot(lsyr):
+    """
+Ne pas modifier, mais on doit modifier pour avoir 10/10 ?
+"""
     title = "Syracuse" + " (n = " + str(lsyr[0]) + " )"
     fig = Figure({  'layout':   { 'title': {'text': title},
                                 'xaxis': {'title': {'text':"x"}},
@@ -14,12 +17,11 @@ def syr_plot(lsyr):
                 }
     )
 
-    x = [ i for i in range(len(lsyr)) ]
+    x = list(range(len(lsyr)))
     t = Scatter(x=x, y=lsyr, mode="lines+markers", marker_color = "blue")
     fig.add_trace(t)
     fig.show()
     # fig.write_html('fig.html', include_plotlyjs='cdn')
-    return None
 #######################
 
 def syracuse_l(n):
@@ -31,9 +33,15 @@ def syracuse_l(n):
     Returns:
         list: la suite de Syracuse de source n
     """
-
-    # votre code ici 
+    # votre code ici
     l = [ ]
+    l.append(n)
+    while n!=1:
+        if n%2==0:
+            n=n/2
+        else:
+            n=n*3+1
+        l.append(n)
     return l
 
 def temps_de_vol(l):
@@ -45,10 +53,9 @@ def temps_de_vol(l):
     Returns:
         int: le temps de vol
     """
-    
     # votre code ici
-
     n = 0
+    n=len(l)-1
     return n
 
 def temps_de_vol_en_altitude(l):
@@ -64,6 +71,13 @@ def temps_de_vol_en_altitude(l):
     # votre code ici
 
     n = 0
+    altitude=l[0]
+    if l[1]<altitude:
+        return 0
+    for j in range(1,len(l)):
+        if l[j]<altitude:
+            return n
+        n=n+1
     return n
 
 
@@ -76,18 +90,13 @@ def altitude_maximale(l):
     Returns:
         int: l'altitude maximale
     """
-    
     # votre code ici
-    
-    n = 0
-    return n
-
-
+    return max(l)
 #### Fonction principale
-
-
 def main():
-
+    """
+Voici la fonction main.
+"""
     # vos appels à la fonction secondaire ici
     lsyr = syracuse_l(15)
     syr_plot(lsyr)
